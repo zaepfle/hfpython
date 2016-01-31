8ee6defc418ec7786fb58bd2bc9554db89984f10
Horst Zaepfle <horst.zaepfle@gmail.com>
2016-01-31 13:20:59 +0100

    fixed syntax error in file access context manager

--- kapitel3/sketch.py
+++ kapitel3/sketch.py
@@ -26,9 +26,9 @@ except IOError:
     print('Datei nicht auffindbar!')
 
 try:
-    while open('mann_datei.txt', 'w') as mann_datei:
+    with open('mann_datei.txt', 'w') as mann_datei:
         print(mann, file=mann_datei)
-    while open('anderer_datei.txt', 'w') as anderer_datei:
+    with open('anderer_datei.txt', 'w') as anderer_datei:
         print(anderer, file=anderer_datei)
 except IOError as err:
     print('Dateifehler: ' + str(err))
